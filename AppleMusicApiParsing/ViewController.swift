04d0014aa54ffdc64b41f7dee402a549ca86ec6b
DmitryDorodniy <dmitrydorodniy@gmail.com>
2022-07-31 17:41:52 +0300

    Initial Commit

---
+++ AppleMusicApiParsing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleMusicApiParsing
+//
+//  Created by Dmitry Dorodniy on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
